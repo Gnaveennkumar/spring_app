41d01c0861ec78d8f6dfc758b8c5e4315fc94f0b
naveen <naveen.kumar55667788@gmail.com>
2023-09-12 09:39:59 +0530

    inaital commit

---
+++ src/main/java/com/tcs/PasswordSecurity.java
@@ -0,0 +1,15 @@
+package com.tcs;
+
+import java.util.Base64;
+import java.util.Base64.Encoder;
+
+public class PasswordSecurity {
+	
+	public String encode(String text) {
+		Encoder encoder = Base64.getEncoder();
+        String encodeToString = encoder.encodeToString(text.getBytes());	
+	             return encodeToString;
+	}
+	
+
+}
